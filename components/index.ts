b8ee7fe4a052bf63a4dc72bcc10b95004dfc76c6
Amang2004 <amangupta.ans@gmail.com>
2025-12-01 16:45:00 +0530

    Add header card component and component exports index

---
+++ components/index.ts
@@ -0,0 +1,29 @@
+// Button Component
+export { Button, type ButtonProps } from './Button';
+export { default as ButtonDefault } from './Button';
+
+// Card Component
+export {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+  type CardProps,
+  type CardHeaderProps,
+  type CardTitleProps,
+  type CardDescriptionProps,
+  type CardContentProps,
+  type CardFooterProps,
+} from './Card';
+export { default as CardDefault } from './Card';
+
+// Input Component
+export { Input, type InputProps } from './Input';
+export { default as InputDefault } from './Input';
+
+// Preview Component
+export { Preview, type PreviewProps } from './Preview';
+export { default as PreviewDefault } from './Preview';
+
