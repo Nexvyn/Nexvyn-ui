9d60226a49c8eeaf846c0f067d0fe0a745dcb9d3
Amang2004 <amangupta.ans@gmail.com>
2025-12-04 03:04:44 +0530

    Clean up footer positioning and whitespace

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,6 @@ import { FooterCard } from "./components/footer";
 
 export default async function Home() {
   const allComps = await getAllComponents();
-
   return (
     <div className=" w-full flex flex-col md:p-2 gap-1 max-w-(--breakpoint-2xl) dark:bg-[#181818]">
       <Navbar />
