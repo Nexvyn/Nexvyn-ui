3972a69f5b59227c3af4ab4ffa3b39c403bcd2dc
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-12-04 00:36:40 +0530

    chore: remove unused imports

--- app/(home)/components/footer.tsx
+++ app/(home)/components/footer.tsx
@@ -1,10 +1,7 @@
 "use client";
 import Link from "next/link";
-import { Github } from "lucide-react";
 import { motion } from "motion/react";
-import VoronoiBackground from "./voronoi-background";
 
-const MotionLink = motion.create(Link);
 export function Footer() {
   return (
     <footer className="relative w-full overflow-hidden px-4 sm:px-6 lg:px-12 py-16 sm:py-20 lg:py-24 rounded-2xl  grain">
