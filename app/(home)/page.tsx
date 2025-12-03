0d4167eb7f2731d1fe713c0a67a9de54d0315ca3
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-12-03 23:29:50 +0530

    feat(components): implement HeroSection and update layout with Navbar and Footer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,15 @@
 import { getAllComponents } from "@/lib/get-components";
-import { LandingHero } from "./components/landing-hero";
 import { Navbar } from "./components/navbar";
-import { FooterCard } from "./components/footer";
+import { HeroSection } from "./components/hero-section";
+import { Footer } from "./components/footer";
 
 export default async function Home() {
   const allComps = await getAllComponents();
 
   return (
-    <div className=" w-full flex flex-col md:p-2 gap-1 max-w-(--breakpoint-2xl) dark:bg-[#181818]">
-      <Navbar />
-      <LandingHero allComps={allComps} />
-      <FooterCard />
+    <div className=" w-full flex flex-col gap-2 md:p-2  max-w-(--breakpoint-2xl) dark:bg-[#181818]">
+      <HeroSection allComps={allComps} />
+      <Footer />
     </div>
   );
 }
