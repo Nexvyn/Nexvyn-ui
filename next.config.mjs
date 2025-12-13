cd14e63bdd1c94aec7d7f0486fac1e0a517d632f
Amang2004 <amangupta.ans@gmail.com>
2025-12-13 14:14:24 +0530

    feat: add next.js configuration with mdx and image optimization

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+import { createMDX } from "fumadocs-mdx/next";
+
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {
+  reactStrictMode: true,
+  devIndicators: false,
+  typescript: {
+    ignoreBuildErrors: false, 
+  },
+  experimental: {
+    optimizePackageImports: [
+      "motion",
+      "@paper-design/shaders-react",
+      "lucide-react",
+      "react-icons/fa6"
+    ],
+  },
+};
+
+export default withMDX(config);
