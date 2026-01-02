215b36c4c59e29432702c160afc3c7a2fe763ee5
Amang2004 <amangupta.ans@gmail.com>
2026-01-02 12:59:15 +0530

    fix: Remove duplicate @types/three dependency from package.json

--- app/(home)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { SearchRegistryProvider } from "@/hooks/use-search-registry";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <SearchRegistryProvider>{children}</SearchRegistryProvider>;
-}
