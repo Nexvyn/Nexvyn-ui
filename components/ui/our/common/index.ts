0f66a45b7f608964ea3df3a5bab33692afa073e9
Amang2004 <amangupta.ans@gmail.com>
2026-01-15 11:45:00 +0530

    fix: resolve all build errors

--- components/ui/our/common/index.ts
+++ components/ui/our/common/index.ts
@@ -1,3 +1,4 @@
-export { Icons } from "./legacy-icon"
+export { Icons } from "@/components/ui/icons/animated/legacy-icon"
 // Note: Static icons (CheckIcon, CopyIcon) are in @/components/ui/icons/static
 // Animated icons are in @/components/ui/icons/animated
+
